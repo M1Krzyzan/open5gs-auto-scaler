ebf86a9daa38ce8551716622822ff9a45e684e92
Michał Krzyżanowski <michal.13777@gmail.com>
2025-05-31 17:14:52 +0200

    added get_upf_pod_name function implementation

--- src/webhook_receiver.py
+++ src/webhook_receiver.py
@@ -1,8 +1,14 @@
 import json
+from typing import Optional
 
+from kubernetes import client, config
 from fastapi import FastAPI, Request
 
 app = FastAPI()
+
+config.load_incluster_config()
+v1 = client.CoreV1Api()
+
 
 @app.post("/alert")
 async def receive_webhook(request: Request):
@@ -16,13 +22,24 @@ async def receive_webhook(request: Request):
     - create patch request to scale UPF pod
     """
     print(payload["alerts"])
-    return {"status":"received"}
+    if (upf_pod_name := get_upf_pod_name()) is None:
+        print("No upf pod in cluster")
+    print(upf_pod_name)
 
-def get_upf_pod_name() -> str:
-    pass
+    return {"status": "received"}
+
+
+def get_upf_pod_name() -> Optional[str]:
+    pod_list = v1.list_pod_for_all_namespaces(watch=False)
+    for pod in pod_list.items:
+        if pod.name.startswith("open5gs-upf"):
+            return pod.metadata.name
+    return None
+
 
 def scale_upf_pod(upf_pod_name: str, cpu_limit: int):
     pass
+
 
 def get_current_upf_cpu_limit(upf_pod_name: str) -> int:
     pass
